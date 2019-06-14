2ae556953b5ed0d618af749a6327b97de9b364a8
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-06-14 16:52:24 -0700

    new class

---
+++ LevelUpGameJam2019/Assets/Scripts/CustomerManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerManager : MonoBehaviour
+{
+    [SerializeField]
+    private Order[] possibleOrders;
+
+    [SerializeField]
+    private Customer[] customersInScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private static int SumOrderWeights(Order[] orders)
+    {
+        var summedWeight = 0;
+
+        foreach(var order in orders)
+        {
+            summedWeight += order.randomWeight;
+        }
+
+        return summedWeight;
+    }
+
+    public Order GetNewOrder()
+    {
+        var randomNumber = Random.Range(0, SumOrderWeights(possibleOrders));
+        
+        Order selectedOrder = null;
+
+        foreach(var order in possibleOrders)
+        {
+            if(randomNumber < order.randomWeight)
+            {
+                selectedOrder = order;
+            }
+
+            else
+            {
+                randomNumber -= order.randomWeight;
+            }
+
+        }
+
+
+        return selectedOrder;
+        
+    }
+}
