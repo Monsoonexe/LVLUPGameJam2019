7081f19b6d2cac59d3f5d2f706acd6acfcb33179
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-06-14 15:13:38 -0700

    Update MouseTracker.cs
    tracks mouse input

--- LevelUpGameJam2019/Assets/Scripts/MouseTracker.cs
+++ LevelUpGameJam2019/Assets/Scripts/MouseTracker.cs
@@ -9,10 +9,28 @@ public class MouseTracker : MonoBehaviour
     [SerializeField]
     private float widthBounds;
 
+    [SerializeField]
+    private float verticalSpeed = 1.0f;
+    [SerializeField]
+    private float horizontalSpeed = 1.0f;
+
+    private Transform myTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GatherReferences();
+
+        ConfigureBounds();
+    }
+
+    private void GatherReferences(){
+
+        myTransform = this.transform;//cache tranny
+    }
+
+    private void ConfigureBounds(){
+
     }
 
     // Update is called once per frame
@@ -23,6 +41,9 @@ public class MouseTracker : MonoBehaviour
         var horizontalInput = Input.GetAxis("Horizontal");
 
         //move tracker
+        myTransform.Translate(0, verticalInput * verticalSpeed * Time.deltaTime, 0);
+
+        myTransform.Translate(horizontalInput * horizontalSpeed * Time.deltaTime, 0, 0);
 
         //keep in bounds
     }
