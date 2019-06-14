1d8c20e24c3dff6317951d95879d874d81b83131
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-06-14 15:05:36 -0700

    new script for mouse input

---
+++ LevelUpGameJam2019/Assets/Scripts/MouseTracker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTracker : MonoBehaviour
+{
+    [SerializeField]
+    private float heightBounds;
+    [SerializeField]
+    private float widthBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get inputs
+        var verticalInput = Input.GetAxis("Vertical");
+        var horizontalInput = Input.GetAxis("Horizontal");
+
+        //move tracker
+
+        //keep in bounds
+    }
+}
