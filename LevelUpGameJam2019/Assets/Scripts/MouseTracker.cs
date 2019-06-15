7f9253e68d38157585007677e581fba7172b7409
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-06-14 21:01:29 -0700

    refixed tracking

--- LevelUpGameJam2019/Assets/Scripts/MouseTracker.cs
+++ LevelUpGameJam2019/Assets/Scripts/MouseTracker.cs
@@ -11,35 +11,41 @@ public class MouseTracker : MonoBehaviour
     private float horizontalSpeed = 1.0f;
 
     private Transform myTransform;
+    private Camera mainCamera;
+
+    private float offset = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         GatherReferences();
 
-        ConfigureBounds();
     }
 
     private void GatherReferences(){
 
         myTransform = this.transform;//cache tranny
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>() as Camera;
     }
 
-    private void ConfigureBounds(){
-
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void TrackMousePosition()
     {
         //get inputs
         var moveVector = Vector3.zero;
         moveVector.x = Input.GetAxis("Horizontal") * horizontalSpeed;
         moveVector.y = Input.GetAxis("Vertical") * verticalSpeed;
+        var mousePos = Input.mousePosition;//get mouse position
 
         myTransform.position += moveVector * Time.deltaTime;
+        var mouseCoords = mainCamera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, offset));//mouse position to screen space, plus an offset to move it in front of cannon
 
+        myTransform.position = mouseCoords;
+    }
 
-        //keep in bounds
+    // Update is called once per frame
+    void Update()
+    {
+        TrackMousePosition();
+
     }
 }
