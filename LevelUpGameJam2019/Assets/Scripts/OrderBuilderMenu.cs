d5b6e38850e17314f581abe7e9a7cf01bae7c2b8
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-06-14 17:30:52 -0700

    Update OrderBuilderMenu.cs
    enum refactor

--- LevelUpGameJam2019/Assets/Scripts/OrderBuilderMenu.cs
+++ LevelUpGameJam2019/Assets/Scripts/OrderBuilderMenu.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OrderBuilderMenu : MonoBehaviour
 {
+    [SerializeField]
+    private Animator ingredientLoaderAnimator;
+
+    //ingredients that are on pizza
+    [SerializeField]
+    private List<IngredientsENUM> selectedIngredients = new List<IngredientsENUM>();
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,31 +23,30 @@ public class OrderBuilderMenu : MonoBehaviour
     {
         
     }
-
+    
     public void AddAnchovies()
     {
-
+        selectedIngredients.Add(IngredientsENUM.Anchovies);
     }
 
     public void AddCheese()
     {
-
+        selectedIngredients.Add(IngredientsENUM.Cheese);
     }
 
     public void AddPepperoni()
     {
-
+        selectedIngredients.Add(IngredientsENUM.Pepperoni);
     }
 
     public void AddSauce()
     {
-
+        selectedIngredients.Add(IngredientsENUM.Sauce);
     }
-
 
     public void AddSausage()
     {
-
+        selectedIngredients.Add(IngredientsENUM.Sausage);
     }
 }
 
