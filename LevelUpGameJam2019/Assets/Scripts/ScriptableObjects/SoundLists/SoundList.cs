d98084a0cfa4a37399931d411b118cadc581763c
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-06-14 22:42:31 -0700

    Organized clips; renamed

---
+++ LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/SoundLists/SoundList.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoundList_", menuName = "ScriptableObjects/New Sound list")]
+public class SoundList : ScriptableObject
+{
+    [Header("---AudioClips---")]
+    public AudioClip[] soundClipList;
+
+    public AudioClip GetRandomSound()
+    {
+        return soundClipList[Random.Range(0, soundClipList.Length)];
+    }
+}
