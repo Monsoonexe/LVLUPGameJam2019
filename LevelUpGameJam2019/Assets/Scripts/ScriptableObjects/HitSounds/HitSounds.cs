d4cb3b6f1bd03cfa7d4ad7fac78a3c5e1a20a4e8
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-06-14 22:25:44 -0700

    new folder, SOs for hits

---
+++ LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/HitSounds/HitSounds.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "HitSounds_", menuName = "ScriptableObjects/New Hit Sounds")]
+public class HitSounds : ScriptableObject
+{
+    [Header("---AudioClips---")]
+    [SerializeField]
+    private AudioClip[] hitSounds;
+
+}
