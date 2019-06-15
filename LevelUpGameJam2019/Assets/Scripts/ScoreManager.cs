36736b111738a85e1fd630bc178f561fc472f2da
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-06-14 21:02:20 -0700

    Update ScoreManager.cs

--- LevelUpGameJam2019/Assets/Scripts/ScoreManager.cs
+++ LevelUpGameJam2019/Assets/Scripts/ScoreManager.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
+    [Header("---Score---")]
+    [SerializeField]
+    private int playerScore;
+
+    [Header("---Tallys---")]
+    [SerializeField]
+    private int ShotsFired;
+
+
     // Start is called before the first frame update
     void Start()
     {
