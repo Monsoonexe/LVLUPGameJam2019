95553552c7b2f5c275605a40c370d41707aa23bc
Matt <mentallic3d@gmail.com>
2019-06-18 17:55:04 -0400

    Final as of deadline

---
+++ LevelUpGameJam2019/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+ 
+    public void PlayGame()
+    {
+        Debug.Log("Playgame");
+        SceneManager.LoadScene("MAIN");
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("MENU");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
