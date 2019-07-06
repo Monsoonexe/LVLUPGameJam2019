a84c091986e22ebd547d5b1752dd22dfec117136
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-05 23:52:47 -0700

    new Leaderboard SO and folder

---
+++ LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/Leaderboards/LeaderboardSO.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HighScores_", menuName = "ScriptableObjects/New High Score")]
+public class LeaderboardSO : ScriptableObject
+{
+    [SerializeField]
+    private int highScore = 0;
+
+    /// <summary>
+    /// [Alpha]
+    /// </summary>
+    [Header("***Leaderboard***")]
+    [Tooltip("Alpha")]
+    [SerializeField]
+    private int[] leaderboardScores = new int[5];
+
+    public void SetNewHighScore(int newScore)
+    {
+        if(newScore > highScore)
+        {
+            highScore = newScore;
+        }
+        else
+        {
+            Debug.LogError("What are you trying to pull? Hax!");
+        }
+    }
+
+    public int GetHighScore()
+    {
+        return highScore;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+        //do more here.
+    }
+}
