906e5982f5c1f1fb010e1ae797bb8add4da12e97
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-08 15:00:51 -0700

    new handles on text elements
    updated prefab with linked references
    
    added functions to read data from score manager

--- LevelUpGameJam2019/Assets/Scripts/UI/LevelEndReadoutController.cs
+++ LevelUpGameJam2019/Assets/Scripts/UI/LevelEndReadoutController.cs
@@ -1,18 +1,77 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
 
 public class LevelEndReadoutController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private ScoreManager scoreManager;
+
+    [Header("---Tally Labels---")]
+    [SerializeField]
+    private TextMeshProUGUI label_customersSatisfied;
+    
+    [SerializeField]
+    private TextMeshProUGUI label_customersMissed;
+    
+    [SerializeField]
+    private TextMeshProUGUI label_customersHit;
+    
+    [SerializeField]
+    private TextMeshProUGUI label_wrongOrders;
+    
+    [SerializeField]
+    private TextMeshProUGUI label_sharksFed;
+    
+    [SerializeField]
+    private TextMeshProUGUI label_piesFired;
+
+    [Space(5)]
+    [Header("---Tally Values---")]
+    [SerializeField]
+    private TextMeshProUGUI value_customersSatisfied;
+    
+    [SerializeField]
+    private TextMeshProUGUI value_customersMissed;
+    
+    [SerializeField]
+    private TextMeshProUGUI value_customersHit;
+    
+    [SerializeField]
+    private TextMeshProUGUI value_wrongOrders;
+    
+    [SerializeField]
+    private TextMeshProUGUI value_sharksFed;
+    
+    [SerializeField]
+    private TextMeshProUGUI value_piesFired;
+
+    private void Start()
     {
-        
+        scoreManager = GameObject.FindGameObjectWithTag("ScoreManager").GetComponent<ScoreManager>() as ScoreManager;
     }
 
-    // Update is called once per frame
-    void Update()
+    [ContextMenu("Load Tally Data")]
+    public void LoadTallyData()
     {
-        
+        this.gameObject.SetActive(true);
+
+        value_customersSatisfied.text = scoreManager.GetTallyCustomersSatisfied().ToString();
+        value_customersMissed.text = scoreManager.GetTallyMissedOrders().ToString();
+        value_customersHit.text = scoreManager.GetTallyCustomersHit().ToString();
+        value_wrongOrders.text = scoreManager.GetTallyIncorrectOrders().ToString();
+        value_sharksFed.text = scoreManager.GetTallySharksFed().ToString();
+        value_piesFired.text = scoreManager.GetTallyShotsFired().ToString();
     }
+
+    public void LoadTallyData(int customersSatisfied, int customersMissed, int customersHit, int wrongOrders, int sharksFed, int shotsFired)
+    {
+        this.gameObject.SetActive(true);
+
+        value_customersSatisfied.text = customersSatisfied.ToString();
+        value_customersMissed.text = customersMissed.ToString();
+        value_customersHit.text = customersHit.ToString();
+        value_wrongOrders.text =wrongOrders.ToString();
+        value_sharksFed.text = sharksFed.ToString();
+        value_piesFired.text = shotsFired.ToString();
+    }
+
 }
