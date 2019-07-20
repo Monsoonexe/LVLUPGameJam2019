c1ab954e92887ee772f3568c23be8e5603db6fc2
Gordon Arber <nodrogames@gmail.com>
2019-07-19 22:38:19 -0400

    Started a dialog system to the Tutorial Scene

---
+++ LevelUpGameJam2019/Assets/Scripts/Tutorial/Dialog.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+    private void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    private void Update()
+    {
+        NextSentence();
+    }
+
+    IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    private void NextSentence()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Next Sentence Please");
+            if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        } else
+        {
+            textDisplay.text = "";
+        }
+        }
+    }
+}
