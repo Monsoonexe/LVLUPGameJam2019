bc02fbff43519454e414c8bf8b427ac83a4d8aa9
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-22 16:09:20 -0700

    new IngredientsSO
    now a pair of ENUM and sprite
    better ToString

---
+++ LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/Ingredients/IngredientSO.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+[CreateAssetMenu(fileName = "Ingredient_", menuName = "ScriptableObjects/New Ingredient")]
+public class IngredientSO : ScriptableObject
+{
+    [SerializeField]//set by Developer
+    private IngredientsENUM ingredient;
+
+    public IngredientsENUM Ingredient { get { return ingredient; } }//externally accessible, readonly
+
+    [SerializeField]//set by Developer
+    private Sprite icon;
+
+    public Sprite Icon { get { return icon; } }//externally accessible, readonly
+
+    /// <summary>
+    /// Prints the Ingredient this represents.
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return ingredient.ToString();
+    }
+}
