2e0d65c677d6ab59de59677862b70da8a7e358cd
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-21 21:19:09 -0700

    new player stats SO and folder

---
+++ LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/PlayerStats/PlayerStats.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerStats_", menuName = "ScriptableObjects/PlayerStats")]
+public class PlayerStats : ScriptableObject
+{
+    [Header("---Player Info---")]
+    public string playerName = "Joe the Pizza Slinger.";
+
+    [Header("---Upgrade Levels---")]
+    public int playerLvel = 1;
+
+    public int pizzaCannonLevel = 1;
+
+    public int pizzaShipLevel = 1;
+
+    [Header("---Money---")]
+    public int money = 0;
+
+    public int premiumMoney = 0;
+
+}
