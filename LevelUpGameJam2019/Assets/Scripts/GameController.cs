04b47b658fb5e6486033e8eed7737122524effc5
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-21 21:26:23 -0700

    refactored to support scene persistence
    implemented unity singleton pattern
    immortality!
    
    no longer tracks player
    now holds prefabs for cannons and ships.

--- LevelUpGameJam2019/Assets/Scripts/GameController.cs
+++ LevelUpGameJam2019/Assets/Scripts/GameController.cs
@@ -1,17 +1,35 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    /// <summary>
+    /// A static instance of a GameController.
+    /// </summary>
+    public static GameController gameController;
+
+    /// <summary>
+    /// LevelData for the level that is currently loaded.
+    /// </summary>
+    private static LevelManager currentLevelManager;
+    
+    [Header("---Player Data---")]
     [SerializeField]
-    private StationaryCannon pizzaCannon;
+    private PlayerStats playerStats;
+    
+    [Header("---Cannons---")]
+    [SerializeField]
+    private GameObject[] cannonPrefabs;
 
     [SerializeField]
-    private GameObject ingredientBuilderCanvas;
+    private int cannonPrefabIndex = 0;
 
+    [Space(10)]
+    [Header("---Ships---")]
     [SerializeField]
-    private GameObject returnToMainMenuPrompt;
+    private GameObject[] shipPrefabs;
+    [SerializeField]
+    private int shipPrefabIndex = 0;
 
     /// <summary>
     /// 
@@ -25,35 +43,84 @@ public class GameController : MonoBehaviour
 
     private void Awake()
     {
-        GatherReferences();
+        InitSingleton(this);
+
+        SceneManager.sceneLoaded += InitLevel;//subscribe to event to know when a scene has changed.
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        returnToMainMenuPrompt.SetActive(false);
+        GatherSceneReferences();
+
+        currentLevelManager.LevelsEndEvent.AddListener(OnLevelsEnd);//subscribe to event to know when level has ended.
+
+        var acceptableShipIndex = Mathf.Clamp(shipPrefabIndex, 0, shipPrefabs.Length - 1);//make sure index is w/n array bounds
+        var acceptableCannonIndex = Mathf.Clamp(cannonPrefabIndex, 0, cannonPrefabs.Length - 1);//make sure index is w/n array bounds
+
+        currentLevelManager.InitLevel(playerStats, shipPrefabs[acceptableShipIndex], cannonPrefabs[acceptableCannonIndex]);//init level
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Handle Quit Game
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             QuitGame();
         }
     }
 
-    private void GatherReferences()
+    private void InitLevel(Scene newScene, LoadSceneMode loadSceneMode)
     {
-        if (!scoreManager)
-        {
-            scoreManager = GameObject.FindGameObjectWithTag("ScoreManager").GetComponent<ScoreManager>() as ScoreManager;
-        }
+        Debug.Log("Init Level shelled...", this);
+        Start();
 
-        if (!pizzaCannon)
+    }
+
+    /// <summary>
+    /// Ensure that only one ever exists in the scene. Sets const static.
+    /// </summary>
+    /// <param name="gameControllerInstance"></param>
+    private static void InitSingleton(GameController gameControllerInstance)
+    {
+        if (!gameController)
         {
-            pizzaCannon = GameObject.FindGameObjectWithTag("Player").GetComponent<StationaryCannon>() as StationaryCannon;
+            gameController = gameControllerInstance;
+            DontDestroyOnLoad(gameController.gameObject);//IMMORTALITY!
         }
+        else//if one already exists
+        {
+            if(gameController != gameControllerInstance)
+            {
+                Destroy(gameControllerInstance.gameObject);//THERE CAN ONLY BE ONE!
+            }
+        }
+    }
+
+    /// <summary>
+    /// Get handles on Monos in the Scene.
+    /// </summary>
+    private void GatherSceneReferences()
+    {
+        GameObject gameObjectQuery;//re-use init
+
+        //handle score manager
+        gameObjectQuery = GameObject.FindGameObjectWithTag("ScoreManager");
+        if (gameObjectQuery)
+            scoreManager = gameObjectQuery.GetComponent<ScoreManager>() as ScoreManager;
+        else
+            Debug.LogError("ERROR! No Score Manager in Scene!");
+
+        //handle level manager
+        gameObjectQuery = GameObject.FindGameObjectWithTag("LevelManager");
+        if (gameObjectQuery)
+            currentLevelManager = gameObjectQuery.GetComponent<LevelManager>() as LevelManager;
+            
+        else
+            Debug.LogError("ERROR! No LevelManager in scene!", this);
+
     }
 
     public void ReloadLevel()
@@ -67,21 +134,13 @@ public class GameController : MonoBehaviour
 
         Application.Quit();//that's all, folks
     }
-
-
+    
     /// <summary>
     /// When the Player reaches the end of the level, this handles showing info to the player and then loading the next scene.
     /// </summary>
     public void OnLevelsEnd()
     {
-        //migrate Player input 
-        //-disable cannon input
-        pizzaCannon.gameObject.SetActive(false);
-        ingredientBuilderCanvas.SetActive(false);
-        //Show player stats
-        //wait for Player to move on to next level
-        scoreManager.OnLevelsEnd();
-        returnToMainMenuPrompt.SetActive(true);
+        //go back to the main menu after awhile.
     }
 
     ///<summary>
