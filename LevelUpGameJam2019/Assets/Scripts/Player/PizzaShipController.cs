bd74320463f5e0dd1123cb7b0a539097ae79cb86
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-21 21:18:21 -0700

    new PizzaShipController
    added to prefab
    removed cannon. replaced with handle to cannon. linked in prefab

---
+++ LevelUpGameJam2019/Assets/Scripts/Player/PizzaShipController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PizzaShipController : MonoBehaviour
+{
+    [SerializeField]//can only set in Inspector
+    private Transform cannonSpawnPoint;
+    public Transform CannonSpawnPoint { get { return cannonSpawnPoint; } }//easy to get, only the Inspector can set.
+            
+}
