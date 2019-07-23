d706215ba29e0b369e01ad98bca7ac872c6da5f0
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-23 01:25:02 -0700

    cleanup

--- LevelUpGameJam2019/Assets/Scripts/TriggerVolume.cs
+++ LevelUpGameJam2019/Assets/Scripts/TriggerVolume.cs
@@ -10,23 +10,30 @@ public class TriggerVolume : MonoBehaviour
     /// <summary>
     /// Event that triggers when the Player enters this volume.
     /// </summary>
+    [Header("---Events---")]
     [SerializeField]//set in Inspector
     [Tooltip("Event that triggers when the Player enters this volume.")]
     private UnityEvent triggerEvent = new UnityEvent();
 
     public UnityEvent Event { get { return triggerEvent; } }//publicly accessible, readonly.
 
+    //external refs
     private static GameObject player;
 
     // Start is called before the first frame update
     void Awake()
+    {
+        GatherReferences();
+    }
+
+    private void GatherReferences()
     {
         if (!player)
         {
             player = GameObject.FindGameObjectWithTag("Player");
         }
     }
-    
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject == player)
