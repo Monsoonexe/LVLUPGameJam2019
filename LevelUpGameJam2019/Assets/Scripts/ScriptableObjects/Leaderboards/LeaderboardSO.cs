74c2943a99d250241a92c3cd121e40ce0cfc2e79
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-25 15:39:10 -0700

    removed Alpha
    capitalizaed HighScore

--- LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/Leaderboards/LeaderboardSO.cs
+++ LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/Leaderboards/LeaderboardSO.cs
@@ -5,13 +5,12 @@ public class LeaderboardSO : ScriptableObject
 {
     private const int defaultLeaderboardSize = 5;
 
-    public int highScore { get { return leaderboardScores[0].score; } }
+    public int HighScore { get { return leaderboardScores[0].score; } }//readonly
 
     /// <summary>
-    /// [Alpha]
+    /// 
     /// </summary>
     [Header("***Leaderboard***")]
-    [Tooltip("[Alpha]")]
     [SerializeField]
     private LeaderboardEntry[] leaderboardScores = new LeaderboardEntry[defaultLeaderboardSize];
 
@@ -55,6 +54,11 @@ public class LeaderboardSO : ScriptableObject
         }
     }
 
+    /// <summary>
+    /// Is the given score in top?
+    /// </summary>
+    /// <param name="playerScore"></param>
+    /// <returns></returns>
     public bool IsScoreOnLeaderboard(int playerScore)
     {
         //is player score greater than lowest score?
