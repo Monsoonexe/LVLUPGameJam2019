23fc776158ce7ab8c6632df40bd3f266ef8c0c1f
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-25 02:58:39 -0700

    new GameEvent and Listener
    scriptable object events!!!

---
+++ LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/GameEvents/GameEvent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameEvent : ScriptableObject
+{
+    private List<GameEventListener> listeners = new List<GameEventListener>();
+
+    /// <summary>
+    /// Call all functions in list.
+    /// </summary>
+    /// <remarks>Calls in reverse order to allow for listeners removing themselves.</remarks>
+    public void Raise()
+    {
+        for(var i = listeners.Count - 1; i >= 0; --i)
+        {
+            listeners[i].OnEventRaised();
+        }
+    }
+
+    public void RegisterListener(GameEventListener listener)
+    {
+        listeners.Add(listener);
+    }
+
+    public void UnregisterListener(GameEventListener listener)
+    {
+        listeners.Remove(listener);
+    }
+    
+}
