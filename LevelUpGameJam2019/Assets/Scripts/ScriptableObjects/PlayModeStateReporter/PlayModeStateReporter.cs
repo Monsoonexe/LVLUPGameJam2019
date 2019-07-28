be078f34bca96bd5d53e5dd64ebd5598431a3709
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-28 15:42:22 -0700

    new PlayModeReporter script that almost works

---
+++ LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/PlayModeStateReporter/PlayModeStateReporter.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// SO that fires events based on clicking Play, Pause, and Exit Button. I SUCK AND I DONT WORK!
+/// </summary>
+[CreateAssetMenu(fileName = "PlayModeStateReporter", menuName = "ScriptableObjects/PlayMode State Reporter")]
+public class PlayModeStateReporter : RichScriptableObject
+{
+    [SerializeField]
+    private PlayModeStateChange currentState;
+
+    [Header("---Game Events---")]
+    [SerializeField]
+    private GameEvent enteredPlayModeEvent;
+
+    [SerializeField]
+    private GameEvent exitPlayModeEvent;
+    
+    [ContextMenu("Initialize Object")]
+    private void Initialize()
+    {
+        Debug.Log(name.ToString() + " has been initialized.  Will only work once.", this);
+        EditorApplication.playModeStateChanged += ModeChanged;
+    }
+
+    public void ModeChanged(PlayModeStateChange newState)
+    {
+        Debug.Log("Changed state from: " + currentState + " to: " + newState);
+        currentState = newState;
+        Initialize();//reload
+
+        switch (currentState)
+        {
+            case PlayModeStateChange.EnteredEditMode:
+                exitPlayModeEvent.Raise();
+                break;
+            case PlayModeStateChange.EnteredPlayMode:
+                enteredPlayModeEvent.Raise();
+                break;
+        }
+    }
+
+}
