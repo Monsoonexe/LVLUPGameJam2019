10e22034128cbb96a15f0171ab7c65e05e919281
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-28 01:00:07 -0700

    added interface
    for generic and non-generic

--- LevelUpGameJam2019/Assets/Scripts/Interfaces/IGameEvent.cs
+++ LevelUpGameJam2019/Assets/Scripts/Interfaces/IGameEvent.cs
@@ -1,8 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿
 
-public interface IGameEvent<T>
+public interface IGameEvent<T>//Generic
 {
-
+    void Raise(T value);
+    void AddListener(IGameEventListener<T> listener);
+    void RemoveListener(IGameEventListener<T> listener);
+    void RemoveAll();
+}
+public interface IGameEvent//paramaterless
+{
+    void Raise();
+    void AddListener(IGameEventListener listener);
+    void RemoveListener(IGameEventListener listener);
+    void RemoveAll();
 }
