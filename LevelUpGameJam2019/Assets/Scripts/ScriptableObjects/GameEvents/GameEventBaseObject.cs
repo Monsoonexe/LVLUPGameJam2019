56035595e51b2a0b9e05a06d22181c2bd6982236
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-28 01:45:20 -0700

    renamed class
    fixed inheritance structure

---
+++ LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/GameEvents/GameEventBaseObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public abstract class GameEventBaseObject : ScriptableObject
+{
+#pragma warning disable 0414
+    [Header("=GameEventBase=")]
+    [SerializeField]
+    [TextArea(1, 3)]
+    private string developerDescription = "Enter a description!";
+#pragma warning restore
+}
