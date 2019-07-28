13947d56d29a245a8328908f187467a63e540a98
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-28 02:12:57 -0700

    renamed

--- LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/GameEvents/RichScriptableObject.cs
+++ LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/GameEvents/RichScriptableObject.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 
-public abstract class GameEventBaseObject : ScriptableObject
+/// <summary>
+/// Gives every SO a description the developers can write notes in.
+/// </summary>
+public abstract class RichScriptableObject : ScriptableObject
 {
 #pragma warning disable 0414
     [Header("=GameEventBase=")]
     [SerializeField]
-    [TextArea(1, 3)]
+    [TextArea(1, 5)]
     private string developerDescription = "Enter a description!";
 #pragma warning restore
 }
