bb2b5baa6c806b5b5d2ee620e89596948aefe60c
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-28 01:26:25 -0700

    Update GameEvent.cs
    removed ability to create instance
    implements new interfaces
    implementation shelled for generic version

--- LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/GameEvents/GameEvent.cs
+++ LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/GameEvents/GameEvent.cs
@@ -1,44 +1,100 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "GameEvent_", menuName = "ScriptableObjects/Game Event")]
-public class GameEvent : ScriptableObject
+public abstract class GameEvent<T> : GameEvent, IGameEvent<T>
 {
-    private readonly List<GameEventListener> listeners = new List<GameEventListener>();
+    private readonly List<IGameEventListener<T>> typedListeners = new List<IGameEventListener<T>>();
 
     [SerializeField]
-    [TextArea]
-    private string DeveloperDescription;
+    protected T debugValue = default;//like saying "null", but for generics
+
+    public void AddListener(IGameEventListener<T> listener)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Raise(T value)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void RemoveAll()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void RemoveListener(IGameEventListener<T> listener)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public abstract class GameEvent : GameEventBase, IGameEvent
+{
+    /// <summary>
+    /// Collection of listeners.
+    /// </summary>
+    protected readonly List<IGameEventListener> listeners = new List<IGameEventListener>();//
+    protected readonly List<System.Action> actions = new List<System.Action>();//function pointers. can handle actions, too
 
     /// <summary>
-    /// Call all functions in list.
+    /// Subscribe a new listener.
     /// </summary>
-    /// <remarks>Calls in reverse order to allow for listeners removing themselves.</remarks>
+    /// <param name="listener"></param>
+    public void AddListener(IGameEventListener listener)
+    {
+        if (!listeners.Contains(listener))
+            listeners.Add(listener);
+    }
+
+    /// <summary>
+    /// Subscribe a new action.
+    /// </summary>
+    /// <param name="action"></param>
+    public void AddListener(System.Action action)
+    {
+        if (!actions.Contains(action))
+            actions.Add(action);
+    }
+
+    /// <summary>
+    /// Call all events that are subscribed to event.
+    /// </summary>
     public void Raise()
     {
-        for(var i = listeners.Count - 1; i >= 0; --i)
-        {
-            listeners[i].OnEventRaised();
-        }
+        //TODO add stack trace
+        for (var i = listeners.Count - 1; i >= 0; --i)//iterate backwards for safely removing self
+            listeners[i].OnEventRaised();//call listener
+        for (var i = actions.Count - 1; i >= 0; --i)
+            actions[i]();//call action
     }
 
-    //make this work!!!
-    //public void Raise(int value)
-    //{
-    //    for (var i = listeners.Count - 1; i >= 0; --i)
-    //    {
-    //        listeners[i].OnEventRaised(value);
-    //    }
-    //}
-
-    public void RegisterListener(GameEventListener listener)
+    /// <summary>
+    /// Remove all actions and listeners.
+    /// </summary>
+    public void RemoveAll()
     {
-        listeners.Add(listener);
+        listeners.RemoveRange(0, listeners.Count);
+        actions.RemoveRange(0, actions.Count);
     }
 
-    public void UnregisterListener(GameEventListener listener)
+    /// <summary>
+    /// Remove given listener.
+    /// </summary>
+    /// <param name="listener"></param>
+    public void RemoveListener(IGameEventListener listener)
     {
-        listeners.Remove(listener);
+        if (listeners.Contains(listener))
+            listeners.Remove(listener);
     }
-    
+
+    /// <summary>
+    /// Removed given action.
+    /// </summary>
+    /// <param name="action"></param>
+    public void RemoveListener(System.Action action)
+    {
+        if (actions.Contains(action))
+            actions.Remove(action);
+    }
 }
