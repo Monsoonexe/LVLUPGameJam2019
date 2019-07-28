51d7307c2ba1473cbd396d89030bcff3bf3b4754
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-28 01:52:02 -0700

    new bool game event script

---
+++ LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/GameEvents/BoolGameEvent.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameEvent_Bool_", menuName = "ScriptableObjects/Game Events/Bool Game Event")]
+public class BoolGameEvent : GameEventBase<bool>
+{
+    //it's an event. it just exists. takes 1 bool parameter.
+}
