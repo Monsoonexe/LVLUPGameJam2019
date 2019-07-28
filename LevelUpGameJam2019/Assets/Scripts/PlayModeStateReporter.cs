f2925da7975240786e68d97d82693819bd98ae06
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-28 16:10:45 -0700

    changed from SO to Mono
    moved folders
    made into prefab

---
+++ LevelUpGameJam2019/Assets/Scripts/PlayModeStateReporter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Fires events based on clicking Play, Pause, and Exit Button.
+/// </summary>
+public class PlayModeStateReporter : MonoBehaviour
+{
+    [Header("= Developer Description =")]
+    [SerializeField]
+    [TextArea]
+    private string description = "Fires events based on clicking Play, Pause, and Exit Button.  Good for demo / testing.";
+
+    [Header("---Game Events---")]
+    [SerializeField]
+    private GameEvent enteredPlayModeEvent;
+
+    [SerializeField]
+    private GameEvent exitPlayModeEvent;
+    
+    private void Awake()
+    {
+        enteredPlayModeEvent.Raise();
+    }
+
+    public void OnApplicationQuit()
+    {
+        exitPlayModeEvent.Raise();
+    }
+
+}
