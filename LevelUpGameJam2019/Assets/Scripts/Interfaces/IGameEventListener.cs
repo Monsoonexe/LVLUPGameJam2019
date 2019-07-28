c7073f9dec2e76c3014c62d578014a5e2f644ff4
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-28 00:57:51 -0700

    added funcs

--- LevelUpGameJam2019/Assets/Scripts/Interfaces/IGameEventListener.cs
+++ LevelUpGameJam2019/Assets/Scripts/Interfaces/IGameEventListener.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿
 public interface IGameEventListener<T>
 {
+    void OnEventRaised(T value);
+}
 
+public interface IGameEventListener
+{
+    void OnEventRaised();
 }
