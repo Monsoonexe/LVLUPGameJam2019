59ca1d90d1d7f526ab35dd0c4127af11fc0adc7d
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-28 01:52:11 -0700

    new game event script

---
+++ LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/GameEvents/GameEvent.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameEvent_", menuName = "ScriptableObjects/Game Events/Game Event")]
+public sealed class GameEvent : GameEventBase
+{
+    //this is an event. it just exists. no parameters.
+}
