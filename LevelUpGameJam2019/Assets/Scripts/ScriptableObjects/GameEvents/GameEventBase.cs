c2e9f49a6be84d460dffdc6362ef4e52b6d72cf9
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-28 01:25:57 -0700

    new base class

---
+++ LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/GameEvents/GameEventBase.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public abstract class GameEventBase : ScriptableObject
+{
+#pragma warning disable 0414
+    [Header("=GameEventBase=")]
+    [SerializeField]
+    [TextArea(1, 3)]
+    private string developerDescription = "Enter a description!";
+#pragma warning restore
+}
