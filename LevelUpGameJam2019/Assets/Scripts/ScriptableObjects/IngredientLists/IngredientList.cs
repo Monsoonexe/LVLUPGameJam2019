9ca23a18c2743093ecdf28b5443835e7d151f820
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-28 18:23:24 -0700

    new Ingredients List SO
    folder, asset

---
+++ LevelUpGameJam2019/Assets/Scripts/ScriptableObjects/IngredientLists/IngredientList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IngredientList_", menuName = "ScriptableObjects/Orders/Ingredient List")]
+public class IngredientList : RichScriptableObject
+{
+    public List<IngredientSO> ingredients = new List<IngredientSO>();
+
+}
